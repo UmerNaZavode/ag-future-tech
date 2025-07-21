a74c4ce12b71d998d5ab9db9951911099727550c
gaba-work <malkocmax@gmail.com>
2025-07-21 18:54:07 +0300

    Feat: Started blog section and need todo tabs

--- front-page.php
+++ front-page.php
@@ -9,6 +9,7 @@ get_header();
 <main>
   <?php get_template_part( 'template-parts/home/hero'); ?>
   <?php get_template_part( 'template-parts/home/features'); ?>
+  <?php get_template_part( 'template-parts/home/blog'); ?>
 </main>
 
 <?php get_footer(); ?>
