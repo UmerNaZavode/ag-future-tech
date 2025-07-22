f09c930996a9449a63b706a5e0eb21ce60ad680c
gaba-work <malkocmax@gmail.com>
2025-07-22 13:09:01 +0300

    Upd: Added blog section to globals

--- front-page.php
+++ front-page.php
@@ -9,7 +9,7 @@ get_header();
 <main>
   <?php get_template_part( 'template-parts/home/hero'); ?>
   <?php get_template_part( 'template-parts/home/features'); ?>
-  <?php get_template_part( 'template-parts/home/blog'); ?>
+  <?php get_template_part( 'template-parts/global/blog'); ?>
   <?php get_template_part( 'template-parts/home/resources'); ?>
   <?php get_template_part( 'template-parts/home/reviews'); ?>
   <?php get_template_part( 'template-parts/global/about'); ?>
--- page-news.php
+++ page-news.php
@@ -9,6 +9,8 @@ get_header();
 
 <main>
   <?php get_template_part( 'template-parts/news/hero-news'); ?>
+  <?php get_template_part( 'template-parts/global/blog'); ?>
+  <?php get_template_part( 'template-parts/global/about'); ?>
 </main>
 
 <?php get_footer(); ?>
