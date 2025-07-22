384c250f8da22d0c09969b168ae2164cc4cd1637
gaba-work <malkocmax@gmail.com>
2025-07-22 13:43:43 +0300

    Feat: Finished videos section - TODO: Fix js

--- page-news.php
+++ page-news.php
@@ -10,6 +10,7 @@ get_header();
 <main>
   <?php get_template_part( 'template-parts/news/hero-news'); ?>
   <?php get_template_part( 'template-parts/global/blog'); ?>
+  <?php get_template_part( 'template-parts/news/videos'); ?>
   <?php get_template_part( 'template-parts/global/about'); ?>
 </main>
 
