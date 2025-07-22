7b6529740ab182a77ab9126bed29f740d3186584
gaba-work <malkocmax@gmail.com>
2025-07-22 11:20:14 +0300

    Feat: Finished reviews section

--- front-page.php
+++ front-page.php
@@ -11,6 +11,7 @@ get_header();
   <?php get_template_part( 'template-parts/home/features'); ?>
   <?php get_template_part( 'template-parts/home/blog'); ?>
   <?php get_template_part( 'template-parts/home/resources'); ?>
+  <?php get_template_part( 'template-parts/home/reviews'); ?>
 </main>
 
 <?php get_footer(); ?>
