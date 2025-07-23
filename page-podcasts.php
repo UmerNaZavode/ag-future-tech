82fa391d516524a11d0b4bcfc120a3bbf7ba1a80
gaba-work <malkocmax@gmail.com>
2025-07-23 13:13:17 +0300

    Feat: Podcasts page added podcasts-videos and about section

--- page-podcasts.php
+++ page-podcasts.php
@@ -9,6 +9,8 @@ get_header();
 
 <main>
   <?php get_template_part( 'template-parts/podcasts/hero-podcasts'); ?>
+  <?php get_template_part( 'template-parts/podcasts/podcasts-videos'); ?>
+  <?php get_template_part( 'template-parts/global/about'); ?>
 </main>
 
 <?php get_footer(); ?>
